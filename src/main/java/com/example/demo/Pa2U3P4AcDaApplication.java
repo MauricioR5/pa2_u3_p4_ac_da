4be2fc72c03dccdf35940592fa8ec6f96693b60f
Alexander Cacuango <amcacuangoa@uce.edu.ec>
2023-07-17 16:30:40 -0500

    Tarea 12

--- src/main/java/com/example/demo/Pa2U3P4AcDaApplication.java
+++ src/main/java/com/example/demo/Pa2U3P4AcDaApplication.java
@@ -1,89 +1,83 @@
 package com.example.demo;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.math.BigDecimal;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.example.demo.repository.modelo.Autor;
-import com.example.demo.repository.modelo.Libro;
-import com.example.demo.service.AutorService;
+import com.example.demo.repository.modelo.CuentaBancaria;
+import com.example.demo.repository.modelo.Propietario;
+import com.example.demo.service.CuentaBancariaService;
+import com.example.demo.service.PropietarioService;
+import com.example.demo.service.TransferenciaService;
 
 @SpringBootApplication
 public class Pa2U3P4AcDaApplication implements CommandLineRunner{
 	
 	@Autowired
-	AutorService autorService;
+	TransferenciaService transferenciaService;
+	
+	@Autowired
+	CuentaBancariaService cuentaBancariaService;
 
+	@Autowired
+	PropietarioService propietarioService;
+	
+
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Pa2U3P4AcDaApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-		System.out.println("Ejemplos de JOIN con Autor");
-		List<Autor> autor1 = this.autorService.buscarAutorJoin();
-		for(Autor autor : autor1) {
-			System.out.println(autor1);
-		}
-		List<Autor> autor2 = this.autorService.buscarAutorLeftJoin();
-		for(Autor autor : autor2) {
-			System.out.println(autor2);
-		}
-		List<Autor> autor3 = this.autorService.buscarAutorRightJoin();
-		for(Autor autor : autor3) {
-			System.out.println(autor3);
-		}
-		List<Autor> autor4 = this.autorService.buscarAutorFullJoin();
-		for(Autor autor : autor4) {
-			System.out.println(autor4);
-		}
-		List<Autor> autor5 = this.autorService.buscarAutorWhereJoin();
-		for(Autor autor : autor5) {
-			System.out.println(autor5);
-		}
 		
-		System.out.println("Ejemplos de JOIN con Libro");
+		//Agregar cuenta
 		
-		List<Libro> libro1 = this.autorService.buscarLibroJoin();
-		for(Libro libro : libro1) {
-			System.out.println(libro1);
-		}
-		List<Libro> libro2 = this.autorService.buscarLibroLeftJoin();
-		for(Libro libro : libro2) {
-			System.out.println(libro2);
-		}
-		List<Libro> libro3 = this.autorService.buscarLibroRightJoin();
-		for(Libro libro: libro3) {
-			System.out.println(libro3);
-		}
-		List<Libro> libro4 = this.autorService.buscarLibroFullJoin();
-		for(Libro libro : libro4) {
-			System.out.println(libro4);
-		}
-		List<Libro> libro5 = this.autorService.buscarLibroWhereJoin();
-		for(Libro libro : libro5) {
-			System.out.println(libro5);
-		}
-		System.out.println("Ejemplos con Fecht Join");
+		Propietario pro1 = new Propietario();
+		pro1.setNombre("Mauricio");
+		pro1.setApellido("Cacuango");
+		pro1.setCedula("1723524664");
+		//this.propietarioService.agregar(pro1);
+
+		CuentaBancaria cuenta1 = new CuentaBancaria();
+		cuenta1.setNumero("220002020");
+		cuenta1.setSaldo(new BigDecimal(100.0));
+		cuenta1.setTipo("Ahorros");
+		cuenta1.setPropietario(pro1);
+		//this.cuentaBancariaService.agregar(cuenta1);
+
+		//Agregar segunda cuenta
 		
-		List<Autor> autor6 = this.autorService.buscarFetchJoin();
-		for(Autor autor : autor6) {
-			System.out.println(autor6);
-		}
+		Propietario pro2 = new Propietario();
+		pro2 .setNombre("Karen");
+		pro2 .setApellido("Jacome");
+		pro2 .setCedula("1715517559");
+		//this.propietarioService.agregar(pro2);
+
+		CuentaBancaria cuenta2 = new CuentaBancaria();
+		cuenta2.setNumero("3325248087");
+		cuenta2.setSaldo(new BigDecimal(150.0));
+		cuenta2.setTipo("Ahorros");
+		cuenta2.setPropietario(pro2);
+		//this.cuentaBancariaService.agregar(cuenta2);	
+
+
+		//Transferencia
+
+		System.out.println("Se realiza transferencia de 10");
+		this.transferenciaService.realizarTransfer("3325248087", "220002020", new BigDecimal(10.0));
 	
-		List<Libro> libro6 = this.autorService.buscarLibroFetchJoin();
-		for(Libro libro : libro6) {
-			System.out.println(libro6);
+		//Reporte
+
+		System.out.println("Reporte Transferencias");
+		System.out.println(this.transferenciaService.reporteTransferencias());
+
 		
-		}
-		
-	}
 	
+}
 }
 
 
