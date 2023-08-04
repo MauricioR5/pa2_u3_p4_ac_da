3fc1486339b225e0cf7ee6d97a80dbb3e9761430
Alexander Cacuango <amcacuangoa@uce.edu.ec>
2023-08-04 12:28:44 -0500

    Tarea_17

--- src/main/java/com/example/demo/funcional/Main.java
+++ src/main/java/com/example/demo/funcional/Main.java
@@ -17,7 +17,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// MetodosReferenciados metodosReferenciados = new MetodosReferenciados();
+
 
 		// Metodos HighOrder
 
